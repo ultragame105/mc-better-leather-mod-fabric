4ed59407e272e9d513d4fea40a2da2e57335924f
Ultragame105 <ultragame105@gmail.com>
2022-11-01 16:52:34 -0400

    Renamed to better suit specific mod goal

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -3,12 +3,22 @@ package net.fabricmc.example;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 
 public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("magic-stuff");
+
+	// an instance of our new item
+	public static final Item CUSTOM_ITEM =
+			Registry.register(Registry.ITEM, new Identifier("magic-stuff", "custom_item_thing"),
+					new Item(new FabricItemSettings().group(ItemGroup.MISC).maxCount(3)));
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +26,7 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+
+		LOGGER.info("Hello Fabric world! I am going!");
 	}
 }
